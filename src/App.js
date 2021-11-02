fae914499f3e99c05cffb298d928a94a2aa2da1b
Sharabu Venkatasairam <VenkataSai.Sharabu@bhavnacorp.com>
2021-11-02 22:58:18 +0530

    todilist project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+
 import React from 'react';
 import './App.css';
 import TodoList from './todolist';
@@ -16,21 +16,8 @@ function App() {
     settodos(nt);
   }
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <div>
+      
     <center>
       
           <h5 id="h">TO DO MANAGEMENT APPLICATION</h5>
